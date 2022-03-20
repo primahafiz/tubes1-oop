645b3141fd0dcf2c7816bfa2dcc81d6f2b11ede2
Claudia <88203931+clauculus@users.noreply.github.com>
2022-03-20 14:41:13 +0700

    crafting

--- Crafting/Crafting.cpp
+++ Crafting/Crafting.cpp
@@ -1,4 +1,5 @@
 #include"Crafting.hpp"
+#include<string>
 
 Crafting::Crafting(){
     for (int i = 0; i < 9; i++){
@@ -6,8 +7,20 @@ Crafting::Crafting(){
     }
 }
 
+/*
+Crafting::Crafting(const Crafting& c){
+    for (int i = 0; i < 9; i++){
+        crafting[i] = SlotCrafting(c.getCrafting(i));
+    }
+}
+*/
+
 Crafting::~Crafting(){
 
+}
+
+Item* Crafting::getCrafting(int i){
+    return crafting[i].getSlotCraftingItem();
 }
 
 // add sebuah Item pada slot crafting ke i
@@ -28,8 +41,58 @@ void Crafting::deleteCrafting(int i){
     }
 }
 
-Item* Crafting::getCrafting(int i){
-    return crafting[i].getSlotCraftingItem();
+// clear crafting
+void Crafting::clearCrafting(){
+    for (int i = 0; i < 9; i++){
+        deleteCrafting(i);
+    }
+}
+
+bool Crafting::isCraftingEmpty(int i){
+    return crafting[i].isSlotCraftingEmpty();
+}
+
+void Crafting::swap(int i, int j){
+    Item *a = getCrafting(j);
+    deleteCrafting(j);
+    addToCrafting(getCrafting(i),j);
+    deleteCrafting(i);
+    addToCrafting(a,i);     
+}
+
+void Crafting::symmetry(){
+    swap(0, 2);
+    swap(3, 5);
+    swap(6, 8);
+}
+
+bool Crafting::isCraftable(string X){
+    string crafting = "";
+    for (int i = 0; i < 9; i++){
+        if (isCraftingEmpty(i)){
+            crafting += "-";
+        } else {
+            crafting += getCrafting(i)->getName();
+        }
+    }
+
+    size_t found = crafting.find(X);
+    /* size_t found2 = crafting.find(X); */
+    if (found != string::npos /*|| found2 != string::npos */){ // kalo ada pattern yang cocok
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int Crafting::totalDurability(){
+    int total = 0;
+    for (int i = 0; i < 9; i++){
+        if(!isCraftingEmpty(i) && getCrafting(i)->isTool()){
+            total += getCrafting(i)->getDurability();
+        }
+    }
+    return total;
 }
 
 void Crafting::printCrafting(){
--- Crafting/Crafting.hpp
+++ Crafting/Crafting.hpp
@@ -10,13 +10,32 @@ class Crafting{
         map<int,SlotCrafting> crafting;
     public:
         Crafting();
+        Crafting(const Crafting& c);
         ~Crafting();
 
+        
         // add sebuah Item pada slot crafting ke i
         void addToCrafting(Item *a, int i);
         // delete sebuah Item pada slot crafting ke i
         void deleteCrafting(int i);
+        // mengambil item pada slot crafting ke i
         Item* getCrafting(int i);
+        // clear crafting
+        void clearCrafting();
+        // cek empty
+        bool isCraftingEmpty(int i);
+
+
+        // swap position item i and j
+        void swap(int i, int j);
+        // membuat resep crafting yang direfleksi terhadap sumbu  y
+        void symmetry();
+        // mengecek apakah craftable
+        bool isCraftable(string X);
+
+        // return total durability dari item yang ada di crafting
+        int totalDurability();
+        
         void printCrafting();
 
 };
