fc34c9de912839f3a48f5680c5d68daabaec258c
Nicholass12 <13520121@std.stei.itb.ac.id>
2022-03-20 22:19:30 +0700

    testing main

--- main.cpp
+++ main.cpp
@@ -3,18 +3,17 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <map>
-#include "Item/Diamond.hpp"
-#include "Item/Item.hpp"
+#include "headd.hpp"
 
 using namespace std;
 
-map<string,int*>recipe;
+map<string, int *> recipe;
 
 // ini nanti pindahin aja ke main.hpp
-void readConfig(){
+void readConfig()
+{
   string configPath = "./config";
-  for (const auto &entry :filesystem::directory_iterator(configPath + "/recipe"))
+  for (const auto &entry : filesystem::directory_iterator(configPath + "/recipe"))
   {
     cout << entry.path() << endl;
     ifstream recipeConfigFile(entry.path());
@@ -29,9 +28,8 @@ void readConfig(){
 int main()
 {
   readConfig();
-
-  Item *k=new Diamond();
-
+  Inventory Itory = Inventory();
+  Crafting Craft = Crafting();
   string configPath = "./config";
   string itemConfigPath = configPath + "/item.txt";
 
@@ -52,95 +50,79 @@ int main()
     // read from file and do something
   }
 
-  // // sample interaction
-  // string command;
-  // while (cin >> command)
-  // {
-  //   if (command == "SHOW")
-  //   {
-  //     for (int i = 0; i < 3; i++)
-  //     {
-  //       for (int j = 0; j < 6; j++)
-  //       {
-  //         if (j < 3)
-  //         {
-  //           cout << "      " << ends;
-  //         }
-  //         else
-  //         {
-  //           cout << "[C " << j - 3 + i * 3 << "] " << ends;
-  //         }
-  //       }
-  //       cout << endl;
-  //     }
-  //     cout << endl;
-  //     for (int i = 0; i < 3; i++)
-  //     {
-  //       for (int j = 0; j < 9; j++)
-  //       {
-  //         if (j + i * 9 > 9)
-  //         {
-  //           cout << "[I" << j + i * 9 << "] " << ends;
-  //         }
-  //         else
-  //         {
-  //           cout << "[I " << j + i * 9 << "] " << ends;
-  //         }
-  //       }
-  //       cout << endl;
-  //     }
-  //   }
-  //   else if (command == "GIVE")
-  //   {
-  //     string itemName;
-  //     int itemQty;
-  //     cin >> itemName >> itemQty;
-  //     cout << "TODO" << endl;
-  //   }
-  //   else if (command == "DISCARD")
-  //   {
-  //   }
-  //   else if (command == "MOVE")
-  //   {
-  //     string slotSrc;
-  //     int slotQty;
-  //     string slotDest;
-  //     // need to handle multiple destinations
-  //     cin >> slotSrc >> slotQty >> slotDest;
-  //     cout << "TODO" << endl;
-  //   }
-  //   else if (command == "USE")
-  //   {
-  //   }
-  //   else if (command == "CRAFT")
-  //   {
-  //     cout << "TODO" << endl;
-  //   }
-  //   else if (command == "EXPORT")
-  //   {
-  //     string outputPath;
-  //     cin >> outputPath;
-  //     ofstream outputFile(outputPath);
+  // sample interaction
+  string command;
+  while (cin >> command)
+  {
+    if (command == "SHOW")
+    {
+      Craft.printCrafting();
+      Itory.printInventory();
+    }
+    else if (command == "GIVE")
+    {
+      string itemName;
+      int itemQty;
+      // Dibandingkan berdasarkan nama benda
+      cin >> itemName >> itemQty;
 
-  //     // hardcode for first test case
-  //     outputFile << "21:10" << endl;
-  //     outputFile << "6:1" << endl;
-  //     for (int i = 2; i < 27; i++)
-  //     {
-  //       outputFile << "0:0" << endl;
-  //     }
+      Item *temp = new Diamond();
+      Itory.addToInventory(temp);
+    }
+    else if (command == "DISCARD")
+    {
+      string itemID;
+      int itemQty;
+      // Dibandingkan berdasarkan ID
+      cin >> itemID >> itemQty;
+      Item *temp = new Diamond();
+      temp.modifyQuantity(-itemQty);
+    }
+    else if (command == "MOVE")
+    {
+      string slotSrc;
+      int slotQty;
+      string slotDest;
+      // need to handle multiple destinations
+      cin >> slotSrc >> slotQty >> slotDest;
+      cout << "TODO" << endl;
+    }
+    else if (command == "USE")
+    {
+      string ToolID;
+      cin >> ToolID;
+      Item *temp = new Diamond();
+      temp.modifyQuantity(-1);
+    }
+    else if (command == "CRAFT")
+    {
+      cout << "TODO" << endl;
+    }
+    else if (command == "EXPORT")
+    {
+      string outputPath;
+      cin >> outputPath;
+      ofstream outputFile(outputPath);
 
-  //     cout << "Exported" << endl;
-  //   }
-  //   else if (command == "EXIT")
-  //   {
-  //     break;
-  //   }
-  //   else
-  //   {
-  //     // todo
-  //     cout << "Invalid command" << endl;
-  //   }
-  // }
-  // return 0;
+      // hardcode for first test case
+      outputFile << "21:10" << endl;
+      outputFile << "6:1" << endl;
+      for (int i = 2; i < 27; i++)
+      {
+        outputFile << "0:0" << endl;
+      }
+
+      cout << "Exported" << endl;
+    }
+    else if (command == "EXIT")
+    {
+      break;
+    }
+    else
+    {
+      // todo
+      cout << "Invalid command" << endl;
+    }
+  }
+  return 0;
 }
